76c2e411ef04fd6396bbf9e8b99488a2664339ad
Renan Oliveira <renan.sergipe68@gmail.com>
2021-08-19 21:50:34 -0300

    Estilizando pagina Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,20 @@
-import {Text} from './style'
+import {Container, Form, SubmitButton} from './style'
+import { FaGithub, FaPlus } from 'react-icons/fa'
 
 export default function Main(){
     return(
-        <Text>Main</Text>
+        <Container>
+             <h1>
+              <FaGithub size={25}/>     
+               My repositories
+            </h1>
+
+             <Form onSubmit={()=>{}} >
+               <input type="text" placeholder="Add repository"/>
+               <SubmitButton>
+               
+               </SubmitButton>
+             </Form>
+        </Container>
     )
 }
--- src/pages/Main/style.js
+++ src/pages/Main/style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
-export const Text = styled.h1`
-color: yellow;
+export const Container = styled.div`
+display: flex;
+
 `;
 
